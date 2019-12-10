a952b73a8d93e34bb1ce13eeeec226c56484e6cb
Alexander Rowland <arowland900@gmail.com>
2019-12-10 09:18:43 -0800

    add show and create routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var hobbyCtrl = require('../controllers/hobbies')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', hobbyCtrl.index);
+router.get('/:id', hobbyCtrl.show);
+router.post('/', hobbyCtrl.new);
 
 module.exports = router;
